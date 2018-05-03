18be0d7318b0d037486d5360663e4e29fe4e0424
SambaLim <limsungho07@hanmail.net>
2018-05-03 15:53:39 +0900

    weather_update

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 
 from flask import Flask, request, jsonify
 
+import re
+import requests
+
 app = Flask(__name__)
 
 '''
@@ -22,17 +25,29 @@ def Keyboard():
 def Message():
 	dataReceive = request.get_json()
 	content = dataReceive['content']
-	
+	# 구로구 구로3동
+	regionCode = '09530540'
+	weather, temp = get_weather(regionCode)
 	if content == u"시작하기":
 		dataSend = {
 			"message" : {
-				"text" : "안녕하세요."
+				"text" : "오늘의 날씨는 {}이고\n온도는 {}℃네요.\n원하는 항목을 선택 혹은 대화창에 적어주세요.".format(weather, temp)
 			}
 		}
 	return jsonify(dataSend)
 
-
-
+# 네이버에서 날씨를 구해오는 방법
+def get_weather(regionCode):
+	url = "https://m.weather.naver.com/m/main.nhn?regionCode=" + regionCode
+	summary_regex = r"weather_set_summary\">(.+?)<br>"
+	nowTemp_regex = r"degree_code full\">(.+?)</em>"
+	response = requests.get(url)
+	data = response.text
+	summary = re.search(summary_regex, data)
+	nowTemp = re.search(nowTemp_regex, data)
+	
+	return summary.group(1), nowTemp.group(1)
+	
 	
 if __name__ == '__main__':
 	app.run()
