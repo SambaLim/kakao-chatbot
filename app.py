d4d6420035a5024da005e31a9cc641469295aa56
SambaLim <limsungho07@hanmail.net>
2018-05-03 16:12:41 +0900

    wrong

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 
 from flask import Flask, request, jsonify
 
-import re
-import requests
-
 app = Flask(__name__)
 
 '''
@@ -23,11 +20,7 @@ def Keyboard():
 
 @app.route('/message', methods=['Post'])
 def Message():
-	
-	# 구로구 구로3동
-	regionCode = '09530540'
-	weather, temp = get_weather(regionCode)
-	
+
 	dataReceive = request.get_json()
 	content = dataReceive['content']
 	
@@ -39,18 +32,6 @@ def Message():
 		}
 	return jsonify(dataSend)
 
-# 네이버에서 날씨를 구해오는 방법
-def get_weather(regionCode):
-	url = "https://m.weather.naver.com/m/main.nhn?regionCode=" + regionCode
-	summary_regex = r"weather_set_summary\">(.+?)<br>"
-	nowTemp_regex = r"degree_code full\">(.+?)</em>"
-	response = requests.get(url)
-	data = response.text
-	summary = re.search(summary_regex, data)
-	nowTemp = re.search(nowTemp_regex, data)
-	
-	return summary.group(1), nowTemp.group(1)
-	
 	
 if __name__ == '__main__':
 	app.run()
