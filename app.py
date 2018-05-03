2d4b9f1ff25141c1df4eb79cd01e4198df37b6c8
SambaLim <limsungho07@hanmail.net>
2018-05-03 15:29:20 +0900

    first test

--- app.py
+++ app.py
@@ -1,9 +1,36 @@
-from flask import Flask
+
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello World!'
+
+@app.route('/keyboard')
+def Keyboard():
+	
+	dataSend = {
+		"type" : "buttons",
+		"buttons" : ["시작하기"]
+	}
+	return jsonify(dataSend)
+
+@app.route('/message', methods=['Post'])
+def Message():
+	dataReceive = request.get_json()
+	content = dataReceive['content']
+	
+	if content == u"시작하기":
+		dataSend = {
+			"message" : {
+				"text" : "안녕하세요."
+			}
+		}
+	return jsonify(dataSend)
+
+
+
 	
 if __name__ == '__main__':
 	app.run()
