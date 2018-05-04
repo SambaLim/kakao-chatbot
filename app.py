7d25550b3f18a9db11a3b1c63b05b8971af4e9d5
SambaLim <limsungho07@hanmail.net>
2018-05-04 16:01:43 +0900

    word_test

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from konlpy.tag import Hannanum
 
 import time
 import re
@@ -6,6 +7,7 @@ import requests
 
 
 app = Flask(__name__)
+hannanum = Hannanum()
 
 '''
 @app.route('/')
@@ -41,20 +43,31 @@ def Message():
 	weather, temp = get_weather(regionCode)
 	winfo = "오늘의 날씨는 " + str(weather) + "이고,\n온도는 " + str(temp) + "℃ 네요."
 	
+	# 형태소 분석 테스트
+	test_h = hannanum.nouns(u"오늘 날씨 어때?")
+	test_h_1 = str(test_h[0])
+	
 	if content == u"시작하기":
 		dataSend = {
 			"message" : {
 				"text" : hello
 			}
 		}
-	elif content == u"날씨":
+	elif content == u"날씨" :
 		dataSend = {
 			"message" : {
 				"text" : winfo
 			}
 		}
+	elif content == u"형태소" :
+		dataSend = {
+			"message" : {
+				"text" : test_h_1
+			}
+		}
 	return jsonify(dataSend)
 
+	
 def get_weather(regionCode):
 	url = "https://m.weather.naver.com/m/main.nhn?regionCode=" + regionCode
 	summary_regex = r"weather_set_summary\">(.+?)<br>"
