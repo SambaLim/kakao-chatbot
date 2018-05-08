8341e773df99baf8d401cfd1b0f48be55e69030c
SambaLim <limsungho07@hanmail.net>
2018-05-08 11:28:04 +0900

    testttt

--- app.py
+++ app.py
@@ -49,12 +49,13 @@ def Message():
 	regionCode = "09530540"
 	weather, temp = get_weather(regionCode)
 	winfo = "오늘의 날씨는 " + str(weather) + "이고,\n온도는 " + str(temp) + "℃ 네요."
+	text = str(content)
 	
 	# 언어분석 json
 	requestJson = {
 		"accessKey" : accessKey,
 		"argument" : {
-			"text" : content,
+			"text" : text,
 			"analysis_code" : analysisCode
 		}
 	}
@@ -78,6 +79,12 @@ def Message():
 		dataSend = {
 			"message" : {
 				"text" : winfo
+			}
+		}
+	elif content == u"테스트"
+		dataSend = {
+			"message" : {
+				"text" : text
 			}
 		}
 	return jsonify(dataSend)
