905887f83858eea1cbd71451a3341dc36fc5791d
SambaLim <limsungho07@hanmail.net>
2018-05-14 09:19:27 +0900

    firestore_add

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ import requests
 import urllib3
 import json
 
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
 
 app = Flask(__name__)
 
@@ -17,6 +21,11 @@ def hello_world():
 
 # 현재 날짜
 nowdate = time.strftime('%y%m%d', time.localtime(time.time()))
+
+# 데이터베이스(firestre) 자체 서버에서 초기화
+cred = credentials.Certificate('C:/Users/limsu/Desktop/Wavus/first-58ff5b88bb57.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 # 입력을 받는 keyboard 부분
 @app.route('/keyboard')
